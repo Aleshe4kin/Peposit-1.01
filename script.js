8df4d64c94c3eaace1e9f2a8d514c03183a93dfb
Aleshe4kin <l2x@mail.ru>
2021-12-13 12:19:56 +0300

    commit4

--- script.js
+++ script.js
@@ -1,68 +1,8 @@
 'use strict';
 
-
-let numberOfFilms;
-
-function start (){
-    numberOfFilms = +prompt ('Сколько фильмов вы просмотрели?');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt ('Сколько фильмов вы просмотрели?');
+function showMe () {
+    for (let i = 1; i < 10; i++){
+        const question = alert (`Я разумный человек! ${i}`);
     }
 }
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-    
-function rememderMyFilms (){
-    let i = 0;
-        while (i<2) {
-        const   a = prompt ('Последний фильм?', ''),
-                b = +prompt ('Какую оценку поставишь?', '');
-                i++;
-            if (a != null && b != null && a != '' && b != '' && a.   length < 50) {
-                    personalMovieDB.movies [a] = b;
-                    console.log('done');
-            } else {
-            console.log('error');
-            i--;
-    }
-    }
-}
-rememderMyFilms ();
-
-    function detectPersonalLevel () {
-        if (personalMovieDB.count < 10) {
-            alert ('Ты посмотрел мало фильмов');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            alert ('Вы классический зритель');
-        } else if (personalMovieDB.count > 30){
-            alert ('Вы киноман!');
-        } else {
-            alert ('Произошла ошибка');
-        }
-    }
-    detectPersonalLevel ();
-
-
-    function showMyDB (hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    }
-    showMyDB (personalMovieDB.privat);
-
-
-    function writeYourGenres () {
-        for (let i = 1; i <= 3; i++) {
-            personalMovieDB.genres[i-1] = prompt (`Ваш любимый жанр под номером ${i} `);
-        }
-    }
-    writeYourGenres ();
-
+showMe ();
